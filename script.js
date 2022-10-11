7ef702f8ec37870a2be2bd68059d262c90b00de8
Proffesor Ghost <101057626+proffesorghost@users.noreply.github.com>
2022-10-12 00:34:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let APIKEY = '641ec2c5b392030a75d78901c8b1292e';
-fetch(`https://api.ipstack.com/check?access_key=${APIKEY}`).then(
+fetch(`http://api.ipstack.com/check?access_key=${APIKEY}`).then(
     res => res.json()
 ).then(data => {
     console.log(data);
@@ -7,10 +7,9 @@ fetch(`https://api.ipstack.com/check?access_key=${APIKEY}`).then(
     document.getElementById('location').innerHTML = '<b>Location: </b>'+ data['city'] +
         '-'+data['zip']+', '+ data['country_name'];
     document.getElementById('continent').innerHTML = '<b>Continent: </b>'+ data['continent_name'];
-    document.getElementById('language').innerHTML = '<b>Language: </b>'
-        + data['languages'].name+'('+ data['languages'].native+')';
     document.getElementById('lati').innerHTML = '<b>Latitude: </b>'+data['latitude'];
     document.getElementById('long').innerHTML = '<b>Longitude: </b>'+data['longitude'];
-    document.getElementById('flag').src = data['country_flag'];
+    document.getElementById('flag').src = data['location']['country_flag'];
+    document.getElementById('language').innerHTML = '<b>Language: </b>' + data['location']['languages'][0]['name'] + '(' + data['location']['languages'][0]['native'] + ')';
 
 })
